470ba028aa538700739a1418ad965e0e7699fa54
Sasha <sanya04588@gmail.com>
2020-01-20 12:21:43 +0200

    Deploy project, fbconfig fix

--- src/app/fbconfig.js
+++ src/app/fbconfig.js
@@ -1,13 +1,13 @@
 import * as firebase from "firebase";
 
 const config = {
-    apiKey: "AIzaSyCpNOv_wze0U4ZLw6LnXh9yf0qLOT6z9Bc",
-    authDomain: "users-firebase-daace.firebaseapp.com",
-    databaseURL: "https://users-firebase-daace.firebaseio.com",
-    projectId: "users-firebase-daace",
-    storageBucket: "users-firebase-daace.appspot.com",
-    messagingSenderId: "18857738986",
-    appId: "1:18857738986:web:b6cb0b8f88725b8f9f0d93"
+    apiKey: "AIzaSyC-zTQRWaBfdsPkgi1Kq90Y-6pBAeMVaFQ",
+    authDomain: "test-for-rabbi.firebaseapp.com",
+    databaseURL: "https://test-for-rabbi.firebaseio.com",
+    projectId: "test-for-rabbi",
+    storageBucket: "test-for-rabbi.appspot.com",
+    messagingSenderId: "1042188674965",
+    appId: "1:1042188674965:web:839eb9ad8747f223b8b89c"
 };
 firebase.initializeApp(config);
 
